1ce82550b24fea060f63ebe561644ed874cb165d
Gertjan Gielen <gjgielen@gmail.com>
2025-06-25 14:48:56 +0200

    Update build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
-REPO="ghcr.io/$docker_username/"
-echo "${REPO}"
-=$(echo "$REPO" | tr '[:upper:]' '[:lower:]')
 
-docker build -t "${LOWERCASE_REPO}micronaut-app:latest" -t "${LOWERCASE_REPO}micronaut-app:1.0-$Tag" app/
+# Verkrijg commit tag of standaard naar 'local'
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+
+# Zet gebruikersnaam en repo volledig in lowercase
+REPO="ghcr.io/${docker_username,,}/"
+echo "$REPO"
+
+docker build \
+  -t "${REPO}micronaut-app:latest" \
+  -t "${REPO}micronaut-app:1.0-${Tag,,}" \
+  app/
