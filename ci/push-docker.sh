5c2adf6a4d1883e43625758fac610944b014eedf
Gertjan Gielen <gjgielen@gmail.com>
2025-06-25 14:50:43 +0200

    Update push-docker.sh

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
-echo "$docker_password" | docker login ghcr.io --username "$docker_username" --password-stdin
-docker push "ghcr.io/$docker_username/micronaut-app:1.0-${GIT_COMMIT::8}" 
-docker push "ghcr.io/$docker_username/micronaut-app:latest" &
+
+# Login met lowercase gebruikersnaam
+echo "$docker_password" | docker login ghcr.io --username "${docker_username,,}" --password-stdin
+
+# Push tags met lowercase gebruikersnaam en commit hash
+docker push "ghcr.io/${docker_username,,}/micronaut-app:1.0-${GIT_COMMIT,,::8}" &
+docker push "ghcr.io/${docker_username,,}/micronaut-app:latest" &
+
 wait
