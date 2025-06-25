2597871535c891fa752e580f38128843c26a0991
Gertjan Gielen <gjgielen@gmail.com>
2025-06-25 14:43:45 +0200

    Update build-docker.sh

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -3,6 +3,6 @@ set -e
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
 REPO="ghcr.io/$docker_username/"
 echo "${REPO}"
-LOWERCASE_REPO=$(echo "$REPO" | tr '[:upper:]' '[:lower:]')
+=$(echo "$REPO" | tr '[:upper:]' '[:lower:]')
 
-docker build -t "${REPO}micronaut-app:latest" -t "${LOWERCASE_REPO}micronaut-app:1.0-$Tag" app/
+docker build -t "${LOWERCASE_REPO}micronaut-app:latest" -t "${LOWERCASE_REPO}micronaut-app:1.0-$Tag" app/
